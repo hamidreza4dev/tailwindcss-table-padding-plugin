be2507d9564dc1b5553b1f6b4df694c6ab153e8d
Hamidreza Firouzi <69403093+hamidreza4dev@users.noreply.github.com>
2023-05-25 14:52:09 +0330

    add left & right utilities and fix some bugs

--- tailwind-table-padding.js
+++ tailwind-table-padding.js
@@ -1,16 +1,19 @@
-const plugin = require('tailwindcss/plugin');
-const theme = require('tailwindcss/defaultTheme');
+const plugin = require("tailwindcss/plugin");
+const theme = require("tailwindcss/defaultTheme");
 
+// prettier-ignore
 const tablePadding = plugin(
   function ({ addUtilities, theme, e }) {
     const values = theme('tablePadding');
     var utilities = Object.entries(values).map(([key, value]) => {
       return {
         [`.${e(`p-table-${key}`)} th, .${e(`p-table-${key}`)} td`]: { padding: `${value}` },
-        [`.${e(`px-table-${key}`)} th`]: { 'padding-left': `${value}`, 'padding-right': `${value}` },
-        [`.${e(`px-table-${key}`)} td`]: { 'padding-left': `${value}`, 'padding-right': `${value}` },
-        [`.${e(`py-table-${key}`)} th`]: { 'padding-top': `${value}`, 'padding-bottom': `${value}` },
-        [`.${e(`py-table-${key}`)} td`]: { 'padding-top': `${value}`, 'padding-bottom': `${value}` },
+        [`.${e(`px-table-${key}`)} th, .${e(`px-table-${key}`)} td`]: { 'padding-left': `${value}`, 'padding-right': `${value}` },
+        [`.${e(`py-table-${key}`)} th, .${e(`py-table-${key}`)} td`]: { 'padding-top': `${value}`, 'padding-bottom': `${value}` },
+        [`.${e(`pl-table-${key}`)} th, .${e(`pl-table-${key}`)} td`]: { 'padding-left': `${value}`},
+        [`.${e(`pr-table-${key}`)} th, .${e(`pr-table-${key}`)} td`]: { 'padding-right': `${value}` },
+        [`.${e(`pt-table-${key}`)} th, .${e(`pt-table-${key}`)} td`]: { 'padding-top': `${value}`},
+        [`.${e(`pb-table-${key}`)} th, .${e(`pb-table-${key}`)} td`]: { 'padding-bottom': `${value}` },
       };
     });
     addUtilities(utilities);
